82e4536c9c8271b6ad8fa9dc7c66833811c0b2a9
Sattar Hummatli <settarxan@gmail.com>
2017-12-06 20:40:29 +0400

    connect receiver to component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
-const API_KEY = '?KEY=sattar123456789sattar';
+const API_KEY = '?key=sattar123456789sattar';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
