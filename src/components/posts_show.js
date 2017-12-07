b3ceedb88c317ff5f1b029bb3f565cac5c2000a2
Sattar Hummatli <settarxan@gmail.com>
2017-12-07 11:25:09 +0400

    add back link to Posts Show Component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
+    //Checks if post in props dont request again
+    // if (!this.props.post) {
+    //   const { id } = this.props.match.params;
+    //   this.props.fetchPost(id);
+    // }
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
@@ -17,6 +23,7 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back To Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
