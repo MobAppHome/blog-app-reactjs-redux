92e3ca9ceb772610f767d36e8e3800bc384436ea
Sattar Hummatli <settarxan@gmail.com>
2017-12-07 07:38:06 +0400

    add form fileds to a Post New

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        Post New!
-      </div>
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
