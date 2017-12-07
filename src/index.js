12f9e6dce75fc39cee82daf64c4070d7ae2cf85a
Sattar Hummatli <settarxan@gmail.com>
2017-12-07 12:45:03 +0400

    prepare for deployment

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
+      <div className="container">
         <Switch>
           <Route path="/posts/new" component={PostsNew}/>
           <Route path="/posts/:id" component={PostsShow}/>
